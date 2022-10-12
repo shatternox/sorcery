63a28c03dd366b1f7ea6b03344ccb36afb4c1804
Ardian Danny <65061019+shatternox@users.noreply.github.com>
2022-10-12 10:06:28 +0700

    lots of updates
    lots of updates

--- website/core/views.py
+++ website/core/views.py
@@ -5,9 +5,19 @@ import os
 core = Blueprint('core', __name__)
 
 
-@core.route("/")
-@core.route("/index")
+@core.route("/", methods=["GET", "POST"])
+@core.route("/index", methods=["GET", "POST"])
 def index():
+
+    if request.method == "POST":
+        image_link = request.form.get("image-link")
+
+        # Do OCR Shits
+        text_result=image_link
+
+        return render_template('search.html', text_result=text_result)
+
+
     return render_template('index.html')
 
 

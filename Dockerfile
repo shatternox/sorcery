aaa23b7091398a26c6bf1dcf74f782eba6cc9095
Ardian Danny <65061019+shatternox@users.noreply.github.com>
2022-10-13 10:21:33 +0700

    update docker
    update docker

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@ RUN apt-get update -y && apt-get install tesseract-ocr -y
 COPY ./requirements.txt /requirements.txt
 RUN pip install -r requirements.txt
 CMD python /sorcery/app.py
+
+
--- website/core/ocrCustomBambang.py
+++ website/core/ocrCustomBambang.py
@@ -9,34 +9,34 @@ def get_img_string(img):
     return result
 
 
-def pdf_to_img(pdf_path):
-    pages = convert_from_path(pdf_path)
-    return pages
+# def pdf_to_img(pdf_path):
+#     pages = convert_from_path(pdf_path)
+#     return pages
 
 
-def get_text_from_pages(pages):
-    text = ''
-    for i in pages:
-        page_separator = (
-            '\n\n========================================\n' +
-            '*** PÁGINA ' +
-            str(pages.index(i) + 1) +
-            '\n========================================\n\n'
-        )
-        page_text = get_img_string(i)
-        text += page_separator + page_text
-    return text
+# def get_text_from_pages(pages):
+#     text = ''
+#     for i in pages:
+#         page_separator = (
+#             '\n\n========================================\n' +
+#             '*** PÁGINA ' +
+#             str(pages.index(i) + 1) +
+#             '\n========================================\n\n'
+#         )
+#         page_text = get_img_string(i)
+#         text += page_separator + page_text
+#     return text
 
 
-def tesseract_test_image():
-    img_path = input('Caminho da imagem de entrada: ')
-    imagem = Image.open(img_path)
-    print('\n*** Lendo imagem: ' + img_path + '\n')
-    print(get_img_string(imagem))
+# def tesseract_test_image():
+#     img_path = input('Caminho da imagem de entrada: ')
+#     imagem = Image.open(img_path)
+#     print('\n*** Lendo imagem: ' + img_path + '\n')
+#     print(get_img_string(imagem))
 
 
-def tesseract_test_pdf():
-    pdf_path = input('Caminho do pdf de entrada: ')
-    print('\n*** Lendo documento: ' + pdf_path + '\n')
-    print(get_text_from_pages(pdf_to_img(pdf_path)))
+# def tesseract_test_pdf():
+#     pdf_path = input('Caminho do pdf de entrada: ')
+#     print('\n*** Lendo documento: ' + pdf_path + '\n')
+#     print(get_text_from_pages(pdf_to_img(pdf_path)))
 
